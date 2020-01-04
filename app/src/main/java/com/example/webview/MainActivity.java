877684a6240203b57c32854bd45674d888988eef
ketanbagade <ketanbagade0@gmail.com>
2020-01-04 17:55:47 +0530

    web  view

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.loadUrl("http://ketanchetan.freeoda.com/ganjichor/index.php");
+    }
+}
